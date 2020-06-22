ba77b73b83c770a1d6033f72cb6167a1c8194b4e
Stanislav Utikeev <utikeev@gmail.com>
2020-06-22 15:53:04 +0300

    Add forgotten commitCharacters API

--- src/vs/workbench/api/common/extHost.protocol.ts
+++ src/vs/workbench/api/common/extHost.protocol.ts
@@ -1133,7 +1133,7 @@ export interface ISuggestDataDto {
 	[ISuggestDataDtoField.insertText]?: string;
 	[ISuggestDataDtoField.insertTextRules]?: modes.CompletionItemInsertTextRule;
 	[ISuggestDataDtoField.range]?: IRange | { insert: IRange, replace: IRange; };
-	[ISuggestDataDtoField.commitCharacters]?: string[];
+	[ISuggestDataDtoField.commitCharacters]?: string[] | { char: string, commitWithoutInsert?: boolean }[];
 	[ISuggestDataDtoField.additionalTextEdits]?: ISingleEditOperation[];
 	[ISuggestDataDtoField.command]?: modes.Command;
 	[ISuggestDataDtoField.kindModifier]?: modes.CompletionItemTag[];
